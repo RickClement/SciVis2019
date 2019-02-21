42b388c32c73bde6e99bd9fba9f1989a5a4ca22a
clementr <clementr.ut.ia@gmail.com>
2019-02-21 16:07:53 +0100

    Fixed skeleton code to allow smoke colouring

--- Smoke/fluids.c
+++ Smoke/fluids.c
@@ -201,9 +201,9 @@ void rainbow(float value,float* R,float* G,float* B)
    const float dx=0.8;
    if (value<0) value=0; if (value>1) value=1;
    value = (6-2*dx)*value+dx;
-   *R = max(0.0,(3-fabs(value-4)-fabs(value-5))/2);
-   *G = max(0.0,(4-fabs(value-2)-fabs(value-4))/2);
-   *B = max(0.0,(3-fabs(value-1)-fabs(value-2))/2);
+   *R = max(1.0,(3-fabs(value-4)-fabs(value-5))/2.0);
+   *G = max(1.0,(4-fabs(value-2)-fabs(value-4))/2.0);
+   *B = max(1.0,(3-fabs(value-1)-fabs(value-2))/2.0);
 }
 
 //set_colormap: Sets three different types of colormaps
