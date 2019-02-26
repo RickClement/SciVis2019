75d962560e582df17bbe27eaa2d263436aa76a3e
clementr <clementr.ut.ia@gmail.com>
2019-02-26 15:15:07 +0100

    Add variable colour no. to greyscale and bands colourmaps

--- Smoke/fluids.c
+++ Smoke/fluids.c
@@ -32,7 +32,7 @@ const int COLOR_RAINBOW=1;
 const int COLOR_BANDS=2;
 int   scalar_col = 0;           //method for scalar coloring
 int   frozen = 0;               //toggles on/off the animation
-int   numcols = 256;			//parameterises the number of colours in the colourmap
+int   numcols = 128;			//parameterises the number of colours in the colourmap
 
 //------ SIMULATION CODE STARTS HERE -----------------------------------------------------------------
 
@@ -224,14 +224,17 @@ void set_colormap(float vy)
 {
    float R,G,B;
 
-   if (scalar_col==COLOR_BLACKWHITE)
+   if (scalar_col==COLOR_BLACKWHITE) {
+       vy *= numcols;
+       vy = (int)(vy); vy/= numcols;
        R = G = B = vy;
+   }
    else if (scalar_col==COLOR_RAINBOW)
        rainbow(vy,&R,&G,&B);
    else if (scalar_col==COLOR_BANDS)
        {
-          const int NLEVELS = 7;
-          vy *= NLEVELS; vy = (int)(vy); vy/= NLEVELS;
+          vy *= numcols;
+          vy = (int)(vy); vy/= numcols;
 	      rainbow(vy,&R,&G,&B);
 	   }
 
@@ -368,8 +371,8 @@ void keyboard(unsigned char key, int x, int y)
 		    if (draw_vecs==0) draw_smoke = 1; break;
 	  case 'm': scalar_col++; if (scalar_col>COLOR_BANDS) scalar_col=COLOR_BLACKWHITE; break;
 	  case 'a': frozen = 1-frozen; break;
-	  case '+': if(numcols < 256) param += 1; break;
-	  case '-': if(numcols > 2)   param -= 1; break;
+	  case '+': if(numcols < 255) numcols += 1; break;
+	  case '-': if(numcols > 1)   numcols -= 1; break;
 	  case 'q': exit(0);
 	}
 }
@@ -421,8 +424,8 @@ int main(int argc, char **argv)
 	printf("y:     toggle drawing hedgehogs on/off\n");
 	printf("m:     toggle thru scalar coloring\n");
 	printf("a:     toggle the animation on/off\n");
-	printf("+      increase colours in the colourmap\n")
-	printf("-      decrease colours in the colourmap\n")
+	printf("+      increase colours in the colourmap\n");
+	printf("-      decrease colours in the colourmap\n");
 	printf("q:     quit\n\n");
 
 
